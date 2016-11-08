e00fcf6dd01b5f564a816cd7f9e0a68769301a7e
Mikael Karlsson <mikaelhakarlsson@gmail.com>
2016-11-08 19:18:43 +0100

    First commit

---
+++ src/main/java/se/umu/cs/c12mkn/App.java
@@ -0,0 +1,13 @@
+package se.umu.cs.c12mkn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
