68055d170ce18aa14ae2f2ab5bb376aa1372cda8
Mikael Karlsson <mikaelhakarlsson@gmail.com>
2016-11-12 11:42:24 +0100

    Implemented message type.

---
+++ src/main/java/se/umu/cs/c12mkn/message/Message.java
@@ -0,0 +1,51 @@
+package se.umu.cs.c12mkn.message;
+
+/**
+ * Created by currybullen on 11/12/16.
+ */
+public class Message {
+    private final String id;
+    private final long timestamp;;
+    private final String sender;
+    private final String topic;
+    private final String content;
+    private final byte[] attachments;
+
+    public Message(String id,
+                   long timestamp,
+                   String sender,
+                   String topic,
+                   String content,
+                   byte[] attachments) {
+        this.id = id;
+        this.timestamp = timestamp;
+        this.sender = sender;
+        this.topic = topic;
+        this.content = content;
+        this.attachments = attachments;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public byte[] getAttachments() {
+        return attachments;
+    }
+}
