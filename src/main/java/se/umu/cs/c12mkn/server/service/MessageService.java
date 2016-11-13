54cb48ee67e705073c279cde25c1debe2359b7a1
Mikael Karlsson <mikaelhakarlsson@gmail.com>
2016-11-13 20:38:04 +0100

    Messageservice.java altered to new design.

--- src/main/java/se/umu/cs/c12mkn/server/service/MessageService.java
+++ src/main/java/se/umu/cs/c12mkn/server/service/MessageService.java
@@ -1,11 +1,13 @@
 package se.umu.cs.c12mkn.server.service;
 
+import com.google.protobuf.ByteString;
 import io.grpc.stub.StreamObserver;
 import se.umu.cs.c12mkn.grpc.*;
-import se.umu.cs.c12mkn.server.security.Challenges;
-import se.umu.cs.c12mkn.server.security.InvalidUserException;
-import se.umu.cs.c12mkn.server.security.NoChallengesException;
+import se.umu.cs.c12mkn.server.security.Secrets;
+import se.umu.cs.c12mkn.server.security.Sessions;
 
+import javax.crypto.SecretKey;
+import java.security.KeyPair;
 import java.util.logging.Logger;
 
 /**
@@ -15,49 +17,44 @@ public class MessageService extends MessageServiceGrpc.MessageServiceImplBase {
     private static final Logger logger = Logger.getLogger(MessageService.class.getName());
 
     @Override
-    public void initAuth(Username username, StreamObserver<Challenge> responseObserver) {
-        try {
-            String challenge = Challenges.getInstance().getChallenge(username.getValue());
-            responseObserver.onNext(Challenge.newBuilder().setValue(challenge).build());
-        } catch (InvalidUserException e) {
-            logger.info(e.getMessage());
-            e.printStackTrace();
-        } catch (NoChallengesException e) {
-            logger.info(e.getMessage());
-            e.printStackTrace();
-        }
-
-        responseObserver.onCompleted();
-    }
-
-    @Override
-    public void postMessage(EncryptedMessage encryptedMessage, StreamObserver<Empty> responseObserver) {
-
+    public void dHKeyExchange(DHParameters dhParameters, StreamObserver<SignedDHResponse> responseObserver) {
+        KeyPair keyPair = Secrets.getInstance().generateKeyPair(dhParameters.getModulus().toByteArray(),
+                dhParameters.getBase().toByteArray());
+        SecretKey secretKey = Secrets.getInstance().generateSecret(keyPair,
+                dhParameters.getPublicKey().toByteArray(),
+                dhParameters.getAlgorithm());
+        String session = Sessions.getInstance().createSession();
+        Secrets.getInstance().addSecret(session, secretKey);
+        PublicKey publicKey = PublicKey.newBuilder().setKey(ByteString.copyFrom(keyPair.getPublic().getEncoded()))
+                .setSession(session)
+                .build();
+//        SignedDHResponse signedDHResponse = SignedDHResponse.newBuilder().setPublicKey(publicKey).
+//                setSign()
     }
 
 //    @Override
-//    public void postMessage(Message message, StreamObserver<Empty> responseObserver) {
-//        se.umu.cs.c12mkn.message.Message messageToBeStored = new se.umu.cs.c12mkn.message.Message(message.getId(),
-//                message.getTimestamp(),
-//                message.getSender(),
-//                message.getTopic(),
-//                message.getContent(),
-//                message.getAttachments().toByteArray());
-//        Database.getInstance().addMessage(messageToBeStored);
-//        responseObserver.onNext(Empty.getDefaultInstance());
+//    public void initAuth(Username username, StreamObserver<Challenge> responseObserver) {
+//        try {
+//            String challenge = Challenges.getInstance().getChallenge(username.getValue());
+//            responseObserver.onNext(Challenge.newBuilder().setValue(challenge).build());
+//        } catch (InvalidUserException e) {
+//            logger.info(e.getMessage());
+//            e.printStackTrace();
+//        } catch (NoChallengesException e) {
+//            logger.info(e.getMessage());
+//            e.printStackTrace();
+//        }
+//
 //        responseObserver.onCompleted();
 //    }
 //
 //    @Override
-//    public void retrieveMessage(Id id, StreamObserver<Message> responseObserver) {
-//        se.umu.cs.c12mkn.message.Message messageFromStorage = Database.getInstance().getMessage(id.getValue());
-//        responseObserver.onNext(Message.newBuilder().setId(messageFromStorage.getId())
-//        .setTimestamp(messageFromStorage.getTimestamp())
-//        .setSender(messageFromStorage.getSender())
-//        .setTopic(messageFromStorage.getTopic())
-//        .setContent(messageFromStorage.getContent())
-//        .setAttachments(ByteString.copyFrom(messageFromStorage.getAttachments()))
-//        .build());
-//        responseObserver.onCompleted();
+//    public void authenticate(Answer answer, StreamObserver<PublicKey> responseObserver) {
+//        Challenges.getInstance().validateAnswer()
+//    }
+//
+//    @Override
+//    public void postMessage(EncryptedMessage encryptedMessage, StreamObserver<Empty> responseObserver) {
+//
 //    }
 }
