cfb67a4e66053d50d3070ee79008734073a41fc0
Mikael Karlsson <mikaelhakarlsson@gmail.com>
2016-11-14 18:13:11 +0100

    DHKeyExchange.java improved, but now has the worst exception handling.

--- src/main/java/se/umu/cs/c12mkn/shared/security/DHKeyExchange.java
+++ src/main/java/se/umu/cs/c12mkn/shared/security/DHKeyExchange.java
@@ -7,31 +7,79 @@ import javax.crypto.interfaces.DHPublicKey;
 import javax.crypto.spec.DHParameterSpec;
 import java.math.BigInteger;
 import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.InvalidParameterSpecException;
+import java.security.spec.X509EncodedKeySpec;
 
 /**
  * Created by currybullen on 11/13/16.
  */
 public class DHKeyExchange {
-    public static KeyPair generateKeyPair(byte[] modulus, byte[] base) throws NoSuchAlgorithmException, InvalidAlgorithmParameterException {
+    public static KeyPair generateKeyPair(byte[] modulus, byte[] base) {
         return generateKeyPair(new BigInteger(modulus), new BigInteger(base));
     }
 
-    public static KeyPair generateKeyPair(BigInteger modulus, BigInteger base) throws NoSuchAlgorithmException, InvalidAlgorithmParameterException {
-        DHParameterSpec dhParameterSpec = new DHParameterSpec(modulus, base);
-        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("DH");
-        keyPairGenerator.initialize(dhParameterSpec);
-        return keyPairGenerator.generateKeyPair();
+    public static KeyPair generateKeyPair(BigInteger modulus, BigInteger base) {
+        try {
+            DHParameterSpec dhParameterSpec = new DHParameterSpec(modulus, base);
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("DH");
+            keyPairGenerator.initialize(dhParameterSpec);
+            return keyPairGenerator.generateKeyPair();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (InvalidAlgorithmParameterException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 
-    public static SecretKey generateSecretKey(Key privateKey, byte[] publicKey, String algorithm) throws NoSuchAlgorithmException, InvalidKeyException {
-        //TODO: Turn byte array into actual key here.
-        return generateSecretKey(privateKey, (Key) null, algorithm);
+    public static SecretKey generateSecretKey(Key privateKey, byte[] publicKey)  {
+        return generateSecretKey(privateKey, bytesToPublicKey(publicKey));
     }
 
-    public static SecretKey generateSecretKey(Key privateKey, Key publicKey, String algorithm) throws NoSuchAlgorithmException, InvalidKeyException {
-        KeyAgreement keyAgreement = KeyAgreement.getInstance("DH");
-        keyAgreement.init((DHPrivateKey) privateKey);
-        keyAgreement.doPhase((DHPublicKey) publicKey, true);
-        return keyAgreement.generateSecret(algorithm);
+    public static SecretKey generateSecretKey(Key privateKey, Key publicKey) {
+        try {
+            KeyAgreement keyAgreement = KeyAgreement.getInstance("DH");
+            keyAgreement.init(privateKey);
+            keyAgreement.doPhase(publicKey, true);
+            return keyAgreement.generateSecret("DES");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static DHParameterSpec generateParameters() {
+        try {
+            AlgorithmParameterGenerator generator = AlgorithmParameterGenerator.getInstance("DH");
+            generator.init(1024);
+            AlgorithmParameters parameters = generator.generateParameters();
+            DHParameterSpec parameterSpec = (DHParameterSpec) parameters.getParameterSpec(DHParameterSpec.class);
+            return parameterSpec;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (InvalidParameterSpecException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    private static PublicKey bytesToPublicKey(byte[] publicKey) {
+        try {
+            X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(publicKey);
+            KeyFactory keyFactory = KeyFactory.getInstance("DH");
+            return keyFactory.generatePublic(x509EncodedKeySpec);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (InvalidKeySpecException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
