cb6081a787e967f9f32651a4c6063df1d6938676
Mikael Karlsson <mikaelhakarlsson@gmail.com>
2016-11-14 18:15:25 +0100

    Implemented a verification object for the client.

---
+++ src/main/java/se/umu/cs/c12mkn/client/security/Verify.java
@@ -0,0 +1,27 @@
+package se.umu.cs.c12mkn.client.security;
+
+import se.umu.cs.c12mkn.client.SessionInfo;
+
+import java.security.*;
+
+/**
+ * Created by c12mkn on 11/14/16.
+ */
+public class Verify {
+    public static boolean verify(byte[] data, byte[] dataSignature) {
+        try {
+            Signature signature = Signature.getInstance("RSA");
+            signature.initVerify(SessionInfo.getInstance().getServerPublicSignKey());
+            signature.update(data);
+            return signature.verify(dataSignature);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (SignatureException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+}
