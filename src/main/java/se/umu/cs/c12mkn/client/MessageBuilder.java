2217690ebd7ef5840562ee057e892b406355a60b
Mikael Karlsson <mikaelhakarlsson@gmail.com>
2016-11-14 18:14:49 +0100

    Implemented a message builder for the client.

---
+++ src/main/java/se/umu/cs/c12mkn/client/MessageBuilder.java
@@ -0,0 +1,34 @@
+package se.umu.cs.c12mkn.client;
+
+import com.google.protobuf.ByteString;
+import se.umu.cs.c12mkn.grpc.DHParameters;
+
+import java.math.BigInteger;
+import java.security.PublicKey;
+
+/**
+ * Created by c12mkn on 11/14/16.
+ */
+public class MessageBuilder {
+    private SessionInfo sessionInfo;
+
+    public MessageBuilder() {
+        sessionInfo = SessionInfo.getInstance();
+    }
+
+    public DHParameters buildDHParameterMessage() {
+        return DHParameters.newBuilder()
+                .setModulus(toByteString(sessionInfo.getDHModulus()))
+                .setBase(toByteString(sessionInfo.getDHBase()))
+                .setPublicKey(toByteString(sessionInfo.getDHPublicKey()))
+                .build();
+    }
+
+    private ByteString toByteString(BigInteger data) {
+        return ByteString.copyFrom(data.toByteArray());
+    }
+
+    private ByteString toByteString(PublicKey publicKey) {
+        return ByteString.copyFrom(publicKey.getEncoded());
+    }
+}
