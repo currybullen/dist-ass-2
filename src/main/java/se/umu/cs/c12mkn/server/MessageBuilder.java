3ff2ccd5eda378bef7190a0a1571d89d19e613f4
Mikael Karlsson <mikaelhakarlsson@gmail.com>
2016-11-15 12:53:54 +0100

    Can now build challenge response messages.

--- src/main/java/se/umu/cs/c12mkn/server/MessageBuilder.java
+++ src/main/java/se/umu/cs/c12mkn/server/MessageBuilder.java
@@ -1,9 +1,13 @@
 package se.umu.cs.c12mkn.server;
 
 import com.google.protobuf.ByteString;
+import se.umu.cs.c12mkn.grpc.Challenge;
 import se.umu.cs.c12mkn.grpc.DHResponse;
+import se.umu.cs.c12mkn.grpc.EncryptedMessage;
 import se.umu.cs.c12mkn.grpc.SignedDHResponse;
+import se.umu.cs.c12mkn.server.security.Sessions;
 import se.umu.cs.c12mkn.server.security.Sign;
+import se.umu.cs.c12mkn.shared.security.Crypt;
 
 import java.security.PublicKey;
 
@@ -21,6 +25,16 @@ public class MessageBuilder {
                 .setDhResponse(dhResponse)
                 .setSign(toByteString(signature))
                 .build();
+    }
+
+    public EncryptedMessage buildChallengeMessage(String challenge, String session) {
+        Challenge message = Challenge.newBuilder().setValue(challenge).build();
+        return buildEncryptedMessage(message.toByteArray(), session);
+    }
+
+    private EncryptedMessage buildEncryptedMessage(byte[] data, String session) {
+        ByteString encryptedData = ByteString.copyFrom(Crypt.encrypt(data, Sessions.getInstance().getSecretKey(session)));
+        return EncryptedMessage.newBuilder().setContents(encryptedData).build();
     }
 
     private ByteString toByteString(PublicKey publicKey) {
