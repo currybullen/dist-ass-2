f1034d54c1601296c0d2d2d7725654aec76947c3
Mikael Karlsson <mikaelhakarlsson@gmail.com>
2016-11-15 12:53:34 +0100

    Added functionality for starting authentication process.

--- src/main/java/se/umu/cs/c12mkn/server/service/MessageService.java
+++ src/main/java/se/umu/cs/c12mkn/server/service/MessageService.java
@@ -1,9 +1,14 @@
 package se.umu.cs.c12mkn.server.service;
 
+import com.google.protobuf.InvalidProtocolBufferException;
 import io.grpc.stub.StreamObserver;
 import se.umu.cs.c12mkn.grpc.*;
 import se.umu.cs.c12mkn.server.MessageBuilder;
+import se.umu.cs.c12mkn.server.security.Challenges;
+import se.umu.cs.c12mkn.server.security.InvalidUserException;
+import se.umu.cs.c12mkn.server.security.NoChallengesException;
 import se.umu.cs.c12mkn.server.security.Sessions;
+import se.umu.cs.c12mkn.shared.security.Crypt;
 import se.umu.cs.c12mkn.shared.security.DHKeyExchange;
 
 import javax.crypto.SecretKey;
@@ -25,6 +30,7 @@ public class MessageService extends MessageServiceGrpc.MessageServiceImplBase {
 
     @Override
     public void dHKeyExchange(DHParameters dhParameters, StreamObserver<SignedDHResponse> responseObserver) {
+        logger.info("DH key exchange request received.");
         KeyPair keyPair = DHKeyExchange.generateKeyPair(
                 dhParameters.getModulus().toByteArray(),
                 dhParameters.getBase().toByteArray());
@@ -35,5 +41,32 @@ public class MessageService extends MessageServiceGrpc.MessageServiceImplBase {
         SignedDHResponse signedDHResponse = messageBuilder.buildSignedDHResponse(keyPair.getPublic(), session);
         responseObserver.onNext(signedDHResponse);
         responseObserver.onCompleted();
+        logger.info("Responded to DH key exchange request.");
+    }
+
+    @Override
+    public void initAuth(EncryptedMessage encryptedMessage, StreamObserver<EncryptedMessage> responseObserver) {
+        String session = encryptedMessage.getSession();
+        logger.info("Authentication intiated by session '" + session + "'.");
+        byte[] decrypted = Crypt.decrypt(encryptedMessage.toByteArray(), getSecretKey(session));
+        try {
+            Username username = Username.parseFrom(decrypted);
+            String challenge = Challenges.getInstance().getChallenge(username.getValue());
+            EncryptedMessage response = messageBuilder.buildChallengeMessage(challenge, session);
+            responseObserver.onNext(response);
+            logger.info("Responded to authentication initiation.");
+        } catch (Exception e) {
+            logger.info("Could not initiate authentication.");
+            e.printStackTrace();
+        } catch (NoChallengesException e) {
+            logger.info("Bad format on authentication request.");
+            e.printStackTrace();
+        }
+
+        responseObserver.onCompleted();
+    }
+
+    private SecretKey getSecretKey(String session) {
+        return Sessions.getInstance().getSecretKey(session);
     }
 }
