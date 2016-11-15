933a4cef631a2e17c59a559d7453947eeafd09fc
Mikael Karlsson <mikaelhakarlsson@gmail.com>
2016-11-15 12:31:39 +0100

    Added functionality for creating Username and AuthResponse messages.

--- src/main/java/se/umu/cs/c12mkn/client/MessageBuilder.java
+++ src/main/java/se/umu/cs/c12mkn/client/MessageBuilder.java
@@ -1,7 +1,11 @@
 package se.umu.cs.c12mkn.client;
 
 import com.google.protobuf.ByteString;
+import se.umu.cs.c12mkn.grpc.AuthResponse;
 import se.umu.cs.c12mkn.grpc.DHParameters;
+import se.umu.cs.c12mkn.grpc.EncryptedMessage;
+import se.umu.cs.c12mkn.grpc.Username;
+import se.umu.cs.c12mkn.shared.security.Crypt;
 
 import java.math.BigInteger;
 import java.security.PublicKey;
@@ -23,6 +27,24 @@ public class MessageBuilder {
                 .setPublicKey(toByteString(sessionInfo.getDHPublicKey()))
                 .setAlgorithm(algorithm)
                 .build();
+    }
+
+    public EncryptedMessage buildUsernameMessage(String username) {
+        Username message = Username.newBuilder().setValue(username).build();
+        return buildEncryptedMessage(message.toByteArray());
+    }
+
+    public EncryptedMessage buildAuthResponseMessage(String username, String challenge, String answer) {
+        AuthResponse message = AuthResponse.newBuilder().setUsername(username)
+                .setChallenge(challenge)
+                .setAnswer(answer)
+                .build();
+        return buildEncryptedMessage(message.toByteArray());
+    }
+
+    private EncryptedMessage buildEncryptedMessage(byte[] data) {
+        ByteString encryptedData = ByteString.copyFrom(Crypt.encrypt(data, sessionInfo.getSecretKey()));
+        return EncryptedMessage.newBuilder().setContents(encryptedData).build();
     }
 
     private ByteString toByteString(BigInteger data) {
