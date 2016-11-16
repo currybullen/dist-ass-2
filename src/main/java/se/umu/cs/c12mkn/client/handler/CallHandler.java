f8c7b820a63b67ba109493f3840cbbe79b93231d
Mikael Karlsson <mikaelhakarlsson@gmail.com>
2016-11-16 16:24:40 +0100

    Added an encryption function.

--- src/main/java/se/umu/cs/c12mkn/client/handler/CallHandler.java
+++ src/main/java/se/umu/cs/c12mkn/client/handler/CallHandler.java
@@ -1,5 +1,6 @@
 package se.umu.cs.c12mkn.client.handler;
 
+import se.umu.cs.c12mkn.client.MessageBuilder;
 import se.umu.cs.c12mkn.client.SessionInfo;
 import se.umu.cs.c12mkn.grpc.EncryptedMessage;
 import se.umu.cs.c12mkn.shared.security.Crypt;
@@ -8,6 +9,21 @@ import se.umu.cs.c12mkn.shared.security.Crypt;
  * Created by currybullen on 11/16/16.
  */
 public class CallHandler {
+    protected EncryptedMessage encryptMessage(byte[] data) {
+        EncryptedMessage encryptedMessage = null;
+
+        if (SessionInfo.getInstance().getAlgorithm().equals("AES")) {
+            byte[] iv = Crypt.generateIV();
+            byte[] encryptedData = Crypt.encryptAES(data, SessionInfo.getInstance().getSecretKey(), iv);
+            encryptedMessage = MessageBuilder.buildEncryptedMessage(encryptedData,
+                    SessionInfo.getInstance().getID(),
+                    SessionInfo.getInstance().getAlgorithm(),
+                    iv);
+        }
+
+        return encryptedMessage;
+    }
+
     protected byte[] decryptMessage(EncryptedMessage encryptedMessage) {
         byte[] data = null;
 
