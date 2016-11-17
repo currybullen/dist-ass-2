a9c168b60937adcfc2d6a4c44cacdb4b5ef4dc3f
Mikael Karlsson <mikaelhakarlsson@gmail.com>
2016-11-17 21:52:00 +0100

    Added functionality for listing nodes.

--- src/main/java/se/umu/cs/c12mkn/server/handler/ListNodesHandler.java
+++ src/main/java/se/umu/cs/c12mkn/server/handler/ListNodesHandler.java
@@ -5,6 +5,9 @@ import se.umu.cs.c12mkn.grpc.Session;
 import se.umu.cs.c12mkn.server.MessageBuilder;
 import se.umu.cs.c12mkn.server.database.Database;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -18,7 +21,12 @@ public class ListNodesHandler extends CallHandler {
         if (!validateSession(session.getValue()))
             return null;
 
-        List<String> nodes = Database.getInstance().getNodes();
+        List<String> nodes = new ArrayList<String>();
+        try {
+            nodes.add(InetAddress.getLocalHost().getHostAddress());
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
         logger.info("Sending node list of length " + nodes.size() + ".");
         return encryptMessage(MessageBuilder.buildNodeList(nodes).toByteArray(),
                 session.getValue());
